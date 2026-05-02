b8be0db43aca996e48fa07c79a9b8dbf8a9d4b66
IT24102701 <it24102701@my.sliit.lk>
2026-05-03 00:20:41 +0530

    bug_fix

---
+++ Product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: [0, 'Price cannot be negative']
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: [0, 'Stock cannot be negative']
+  },
+  imageUrl: {
+    type: String,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  timestamps: true
+});
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
